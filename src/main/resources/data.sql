e174dad7bee9e379116f95f79b3253c75e00555e
nannigeri <annigeri.n@northeastern.edu>
2024-08-20 18:06:24 +0000

    database additions

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('Neha', 'pass', 'neha@a.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('Dzejla', 'pass', 'dzjela@b.com');
+insert into CUSTOMERS (CUSTOMER_NAME, PASSWORD, EMAIL) VALUES ('Chris', 'pass', 'chris@c.com');
