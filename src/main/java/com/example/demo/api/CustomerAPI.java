da824b3756feefd86c425844190e56b20f0bec01
jlasgithub <da33@njit.edu>
2024-08-22 13:41:55 +0000

    few changes to enable routing with frontend

--- src/main/java/com/example/demo/api/CustomerAPI.java
+++ src/main/java/com/example/demo/api/CustomerAPI.java
@@ -8,6 +8,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,11 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.example.demo.domain.Customer;
 import com.example.demo.repository.CustomersRepository;
 import com.example.demo.domain.Customer;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/customers")
 public class CustomerAPI {
     // initialize repo
